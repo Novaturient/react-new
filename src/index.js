ae464b1af46faf6cf3d5c1f3d1314c68b061da20
Rofel Valeros <rofelvaleros@gmail.com>
2019-10-08 20:35:51 +0800

    cleanup useEffects

--- src/index.js
+++ src/index.js
@@ -42,13 +42,7 @@ const Notes = () => {
             <h1>My Notes</h1>
             {/* convert array of objects into an array of jsx */}
             {notes.map((note) => (
-                <div key={note.title}>
-                    <h3>{note.title}</h3>
-                    <p>{note.body}</p>
-                    {/* <button onClick={() => removeNote(note.title)}>x</button> */}
-                    <button onClick={() => removeNote(note.title)}>x</button>
-                </div>
-
+                <Note key={note.title} note={note} removeNote={removeNote}/>
             ))}
             <p>Add Notes</p>
             <form onSubmit={addNote}>
@@ -61,6 +55,27 @@ const Notes = () => {
     )
 }
 
+//args = destructured props
+const Note = ({note, removeNote}) => {
+
+    useEffect(() => {
+        console.log("aloha")
+
+        //use a function to clean up effects - componentDidUnmount
+        return () => {
+            console.log("cleaning up effect")
+        }
+    },[])
+
+    return (
+        <div key={note.title}>
+            <h3>{note.title}</h3>
+            <p>{note.body}</p>
+            {/* <button onClick={() => removeNote(note.title)}>x</button> */}
+            <button onClick={() => removeNote(note.title)}>x</button>
+        </div>
+    )
+}
 
 // const App = (props) => {
 
